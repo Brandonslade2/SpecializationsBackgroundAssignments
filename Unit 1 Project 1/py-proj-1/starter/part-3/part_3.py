d3908e339011889de6d223dea7a4bfa9d56e26a9
Jay Slade <brandonslade2@gmail.com>
2022-10-30 22:39:21 -0700

    I am now on Object Oriented Programming section in Frodo

--- Unit 1 Project 1/py-proj-1/starter/part-3/part_3.py
+++ Unit 1 Project 1/py-proj-1/starter/part-3/part_3.py
@@ -1,5 +1,5 @@
 my_book = {
-    "title": "Then Hunger Games",
+    "title": "The Hunger Games",
     "author": "Suzanne Collins",
     "year": 2008,
     "rating": 4.32,
@@ -10,7 +10,12 @@ my_book = {
 
 # Code below
 
+def mybook (a):
+    string = f"uh"
+    print(string)
+    return string
 
+mybook(my_book)
 
 
 # Once you are finished with that function, create several more functions which return individual pieces of information from the book.
@@ -18,6 +23,33 @@ my_book = {
 # Code below
 
 
+
 # Finally, create at least three new functions that might be useful as we expand our home library app. Perhaps thing of a way you could accept additional arguments when the function is called? Also, imagine you have a list filled with dictionaries like above.
 
 # Code below
+
+def mybooktitle(my_book):
+    title = my_book['title']
+    print(title)
+
+def mybookauthor (my_book):
+    author = my_book['author']
+    print(author)
+
+def mybookyear (my_book):
+    year = my_book['year']
+    print(year)
+
+def mybookrating (my_book):
+    rating = my_book['rating']
+    print(rating)
+
+def mybookpages (my_book):
+    pages = my_book['pages']
+    print(pages)
+
+mybooktitle(my_book)
+mybookauthor(my_book)
+mybookyear(my_book)
+mybookrating(my_book)
+mybookpages(my_book)
